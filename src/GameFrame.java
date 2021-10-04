cb35d9cffb84a3aa294219ff766aec7fe6e1bd20
Dodsy <halseyng@outlook.com>
2021-10-04 01:08:15 -0400

    cleaning some stuff up

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,7 +3,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.ArrayList;
 import javax.swing.event.MouseInputAdapter;
 
 public class GameFrame extends JFrame{
