5bc97c4cdd72f19bd7be042bacad647f01929dc7
Dodsy <halseyng@outlook.com>
2021-10-04 01:05:58 -0400

    separating the GUI from the logic a bit better for readability

--- src/Board.java
+++ src/Board.java
@@ -1,85 +1,36 @@
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 import java.util.ArrayList;
-
-import javax.swing.event.MouseInputAdapter;
 public class Board{
-   
-    private JLabel[][] labels;
-    private boolean xTurn;
-    private boolean gameOver;
+    private boolean isXTurn;
+    private boolean isFinished;
     private int[][] board;
     private ArrayList<int[]> winSet;
     public Board(){
-        xTurn = true;
-        gameOver = false;
+
+        isXTurn = true;
+        isFinished = false;
+
         winSet = new ArrayList<>();
         board = new int[3][3];
     }
-    public void createLabels(ImageImplement panel){
-        labels = new JLabel[3][3];
-        for (int i = 0; i < labels.length; i++){
-            for (int j = 0; j < labels.length; j++){
-                final int x_coord = i;
-                final int y_coord = j;
-                labels[i][j] = new JLabel();
-                labels[i][j].setForeground(Color.black);
-                labels[i][j].addKeyListener(new KeyAdapter(){
-                    public void keyPressed(KeyEvent e){
-                        int key = e.getKeyCode();
-                        if (!gameOver){ 
-                            if (key == 88){
-                                if (xTurn){
-                                    labels[x_coord][y_coord].setText("X");
-                                    labels[x_coord][y_coord].setEnabled(false);
-                                    board[x_coord][y_coord] = 1;
-                                    checkWin(x_coord,y_coord);
-                                    xTurn = !xTurn;
-                                }
-                            }
-                            if (key == 79){
-                                if (!xTurn){
-                                    labels[x_coord][y_coord].setText("O");
-                                    labels[x_coord][y_coord].setEnabled(false);
-                                    board[x_coord][y_coord] = -1;
-                                    checkWin(x_coord,y_coord);
-                                    xTurn = !xTurn;
-                                }
-                            }
-                        }
-                    }
-                    
-                });
-                
-                labels[i][j].addMouseListener(new MouseInputAdapter(){
-                    @Override
-                    public void mousePressed(MouseEvent e) {
-                        labels[x_coord][y_coord].requestFocus();  
-                    }
-                });
-                
-                labels[i][j].setFocusable(true);
-                labels[i][j].setFont(new Font("Comic Sans",Font.BOLD,50));
-                labels[i][j].setHorizontalAlignment(JLabel.CENTER);
-                labels[i][j].setBounds(43+(i*143),30+(j*136),136,133);
-                panel.add(labels[i][j]);
-
-
-            }
-
+    public void put(int x, int y){
+        board[x][y] = isXTurn ? 1 : -1;
+        if (checkWin(x,y)){
+            isFinished = true;
         }
+        isXTurn = !isXTurn;
     }
-    // we could extend our code later to check for a win and make a new board
-    public void checkWin(int x, int y){
-        int target = xTurn ? 3 : -3;
-        if (checkVertical(target,y) || checkHorizontal(target,x) || checkDiagonal(target,x,y)){
-            for (int[] win : winSet){
-                labels[win[0]][win[1]].setForeground(Color.RED);
-                gameOver = true;
-            }
-        }
+    public ArrayList<int[]> getWinSet(){
+        return winSet;
+    }
+    public boolean isFinished(){
+        return isFinished;
+    }
+    public boolean isXTurn(){
+        return isXTurn;
+    }
+    public boolean checkWin(int x, int y){
+        int target = isXTurn ? 3 : -3;
+        return (checkVertical(target,y) || checkHorizontal(target,x) || checkDiagonal(target,x,y));
     }
     private boolean checkHorizontal(int target, int x){
         for (int i = 0; i < 3; i++){
--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.event.MouseInputAdapter;
 
 public class GameFrame extends JFrame{
     private String mainMenuPath = "assets/mainMenu.png";
@@ -77,14 +81,65 @@ public class GameFrame extends JFrame{
     }
     public class GamePanel extends JPanel{
         private ImageImplement boardImage;
+        private JLabel[][] labels;
         public GamePanel(Board board){
             boardImage = new ImageImplement(new ImageIcon("assets/board.png").getImage());
             add(boardImage);
-            board.createLabels(boardImage);
+            createLabels(boardImage);
             repaint();
             setSize(500,600);
             setLayout(null);
             setVisible(true);
+        }
+        public void createLabels(ImageImplement panel){
+            labels = new JLabel[3][3];
+            for (int i = 0; i < labels.length; i++){
+                for (int j = 0; j < labels.length; j++){
+                    final int x_coord = i;
+                    final int y_coord = j;
+                    labels[i][j] = new JLabel();
+                    createListeners(x_coord,y_coord);
+                    labels[i][j].setForeground(Color.black);
+                    labels[i][j].setFont(new Font("Comic Sans",Font.BOLD,50));
+                    labels[i][j].setFocusable(true);
+                    labels[i][j].setHorizontalAlignment(JLabel.CENTER);
+                    labels[i][j].setBounds(43+(i*143),30+(j*136),136,133);
+                    panel.add(labels[i][j]);
+                }
+            }
+        }
+        public void createListeners(int x_coord, int y_coord){
+            labels[x_coord][y_coord].addKeyListener(new KeyAdapter(){
+                public void keyPressed(KeyEvent e){
+                    int key = e.getKeyCode();
+                    if (!board.isFinished()){ 
+                        if (key == 88 || key == 79){
+                            if (board.isXTurn()){
+                                labels[x_coord][y_coord].setText("X");
+                                labels[x_coord][y_coord].setEnabled(false);
+                                board.put(x_coord,y_coord);
+                            } else {
+                                labels[x_coord][y_coord].setText("O");
+                                labels[x_coord][y_coord].setEnabled(false);
+                                board.put(x_coord,y_coord);
+                            }
+                        }
+                        if (board.isFinished()){
+                            for (int[] win : board.getWinSet()){
+                                labels[win[0]][win[1]].setForeground(Color.red);
+                            }
+                        }
+                    }
+                }
+                
+            });
+            
+            labels[x_coord][y_coord].addMouseListener(new MouseInputAdapter(){
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    labels[x_coord][y_coord].requestFocus();  
+                }
+            });
         }
     }
     public class MenuBar extends JMenuBar{
